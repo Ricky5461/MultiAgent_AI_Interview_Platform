7c03500ffbeff46712c8c63966a53edee39c32d0
Ricky5461 <rickykumarmth000@gmail.com>
2026-09-01 20:02:09 +0530

    Interview coins wanted to resete but earlier i directly went to mongodb atlas and updated there useCoins but in test folder instead of updating in user folder , i tried so many attempt from power shell it was failed due to this miss folder, i have flushed all session stored in cookies in redis as it store session data,So when you need to updated you can update this coin from powershell as well or in same directory of mongodb atlas  , by installing mongosh in terminal and b writing some cmd prompt , i build reseteCoins in contorller and route use-coin in route

--- 2.freshAI/frontend/src/pages/InterviewPage.jsx
+++ 2.freshAI/frontend/src/pages/InterviewPage.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 
 function InterviewPage() {
   
-  return (
+const { id } = useParams()
+const [ loading , setLoading ] = useState(true);
+const [interview, setInterview] = useState(null)
+const navigate = useNavigate()
+
+useEffect(()=>{
+  const fetchInterview = async ()=> {
+     
+  }
+},[id,navigate])
+
+return (
     <div className='text-black'>
       Hello from Interview page
     </div>
